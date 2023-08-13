fd520416abb18dea0a46e19af0691767ca1865f0
qihguang <qihguang@163.com>
2023-08-13 10:49:39 +0800

    commit_all

--- code_1.py
+++ code_1.py
@@ -4,3 +4,6 @@ def print_f(times):
         print(f"光启宏 + 输入次数 + {i}")
 
 print_f(5)
+
+def print_a():
+    print("flowers")
---
+++ code_2.py
@@ -0,0 +1,7 @@
+
+def print_f(times):
+    for i in range(times):
+        print(f"桂德娇 + 输入次数 + {i}")
+
+def cry():
+    print("wuwuwuwuwu")
